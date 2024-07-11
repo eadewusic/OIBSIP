1ab7aa595aabf2c13c2e79bd590d8cfc18d91026
Eunice Adewusi <e.adewusi@alustudent.com>
2024-07-11 13:55:12 +0200

    bug resolved: browser waiting to load + used ID instead of class

--- oibsip_task2/selenium_web.py
+++ oibsip_task2/selenium_web.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By #to search google search field
 from selenium.webdriver.common.keys import Keys #to send specific key that can't be typed like the arrow keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 #download chrome webdriver = https://sites.google.com/chromium.org/driver/
 class info():
@@ -14,26 +15,22 @@ class info():
         # Initiate the Chrome driver using the service obj
         self.driver = webdriver.Chrome(service=self.service)
     
-#task performing main function, query is what you want to search
+   #task performing main function, query is what you want to search
     def get_info(self, query):
         self.query = query
         self.driver.get("https://google.com") #self.driver initiates the chromedriver, .get method takes the url for a search
-        self.driver.implicitly_wait(15)  # Increased wait time
 
         try:
-        # Wait for the search box to appear
-            search = WebDriverWait(self.driver, 20).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
-            )
-        
-        # Clear the search box and enter the query
-            search.clear()
-            search.send_keys(query + Keys.ENTER)
+        # Search for the element and interact with it
+            search = self.driver.find_element(By.ID, "APjFqb") #triggers search box on Google page and stores it in the search variable
+            search.clear() #to clear whatever may initially be in the searchbox
+            search.send_keys(query + Keys.ENTER) #enter the query in the searchbox + trigger to hit enter so we can search
+            time.sleep(10)  # Keep the browser open for 10 seconds
+            self.driver.quit()  # Close the browser after waiting
 
         except Exception as e:
             print(f"An error occurred: {e}")  # Print the error message for debugging
 
-
 #class instance
 assist = info()
-assist.get_info('neutron')
+assist.get_info('Eunice Adewusi Climiradi')
