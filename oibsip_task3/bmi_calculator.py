a511717439caaa1e5b1eb1e5aabcb12cabd051e9
Eunice Adewusi <e.adewusi@alustudent.com>
2024-07-17 00:51:16 +0200

    added exit function

--- oibsip_task3/bmi_calculator.py
+++ oibsip_task3/bmi_calculator.py
@@ -1,10 +1,11 @@
 # BMI = (weight in pounds-lbs x 703) / (height in inches x height in inches)
+# if you're using weight in kilograms and height in meters, there's no need to multiply by 703
 
 name = input("What is your name? ")
-print("Welcome to the BMI Calculator,",name)
+print("Welcome to the BMI Calculator, {name}")
 weight = int(input("Enter your weight in kilograms: "))
 height = int(input("Enter your height in metres: "))
-BMI = (weight * 703) / (height * height)
+BMI = (weight) / (height ** 2)
  
 print("Your BMI is:",BMI)
 
@@ -35,3 +36,12 @@ if BMI>0:
 
 else:
     print("Enter a valid input")
+
+while True:
+    exit_prompt = input("Do you want to calculate another BMI? (yes/no): ").strip().lower()
+    if exit_prompt in ('yes', 'no'):
+        break
+    print("Invalid input. Please enter 'yes' or 'no'.")
+    
+if exit_prompt == 'no':
+    print("Thank you for using the BMI Calculator. Goodbye!")
