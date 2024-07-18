823e5733fa14da43338164c9d321c6261d5cd0ad
Eunice Adewusi <e.adewusi@alustudent.com>
2024-07-18 13:05:37 +0200

    added comments

--- oibsip_task3/bmi_calculator.py
+++ oibsip_task3/bmi_calculator.py
@@ -1,12 +1,12 @@
-# BMI = (weight in pounds-lbs x 703) / (height in inches x height in inches)
+# BMI = (weight in pounds-lbs x 703) / (height in inches x height in inches or do height ** 2)
 # if you're using weight in kilograms and height in meters, there's no need to multiply by 703
 
-def get_user_input(prompt, input_type=float):
+def get_user_input(prompt, input_type=float): #get_user_input is a helper function that simplifies the process of getting and validating user input while input_type parameter converts the user's input (initially a string) to a decimal number (float).
     while True:
         try:
             value = input_type(input(prompt))
             if value <= 0:
-                raise ValueError
+                raise ValueError #this statement is used to trigger an exception if the condition is not met
             return value
         except ValueError:
             print("Invalid input. Please enter a positive number.")
@@ -32,20 +32,20 @@ def classify_bmi(bmi):
     else:
         return "Obesity (Class III)"
 
-def main():
+def bmi_calculator(): #this function contains the primary logic of the BMI program
     print("Welcome to the BMI Calculator")
 
-    while True:
+    while True: #will break if the user chooses to exit the BMI program
         name = input("What is your name? ")
         print(f"Hello, {name}")
         
-        weight = get_user_input("Enter your weight in kilograms: ")
-        height = get_user_input("Enter your height in meters: ")
+        weight = get_user_input("Enter your weight in kilograms: ") #prompts for and validates the user's weight.
+        height = get_user_input("Enter your height in meters: ") #prompts for and validates the user's height.
         
         bmi = calculate_bmi(weight, height)
         bmi_category = classify_bmi(bmi)
         
-        print(f"Your BMI is: {bmi:.2f}")
+        print(f"Your BMI is: {bmi:.2f}") #prints the BMI value formatted to 2 decimal places
         print(f"Dear {name}, you are classified as '{bmi_category}'")
         
         while True:
@@ -58,5 +58,5 @@ def main():
             print("Thank you for using the BMI Calculator. Goodbye!")
             break
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": #condition checks if the script is being run directly. If it is, the block of code inside the if statement is executed, allowing the script to be used both as a standalone program and as an importable module
+    bmi_calculator()
